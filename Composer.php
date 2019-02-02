7cb6a1f5a681d44779546448a6a458db59b24880
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-02-02 20:50:55 +0530

    initial commit

---
+++ Composer.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Class Composer
+ */
+class Composer
+{
+    /**
+     * @var string
+     */
+    protected $binPath = '/bin';
+
+    /**
+     * @var string
+     */
+    protected $rootPath = '/';
+
+    /**
+     * Composer constructor.
+     *
+     * @param  string $projectRootPath
+     * @param  string $binPath
+     */
+    public function __construct(string $projectRootPath, string $binPath)
+    {
+        $this->rootPath = $projectRootPath;
+        $this->binPath  = $binPath;
+    }
+
+    /**
+     * Determines whether composer is installed in the project.
+     *
+     * @return bool
+     */
+    public function exists(): bool
+    {
+        return file_exists($this->binPath . "/composer") ? true : false;
+    }
+
+    /**
+     * Installs composer in the project.
+     *
+     * @return void
+     */
+    public function install(): void
+    {
+        $CURRENT_WORKING_DIRECTORY = getcwd();
+
+        chdir($this->rootPath);
+
+        $MAX_EXECUTION_TIME = 1800; // "30 Mins" for slow internet connections.
+
+        set_time_limit($MAX_EXECUTION_TIME);
+
+        shell_exec("php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\"");
+        shell_exec("php composer-setup.php --install-dir=" . $this->binPath . " --filename=composer");
+        shell_exec("php -r \"unlink('composer-setup.php');\"");
+
+        chdir($CURRENT_WORKING_DIRECTORY);
+    }
+
+    /**
+     * Installs the packages present in the composer.json file.
+     *
+     * @return void
+     */
+    public function installPackages(): void
+    {
+        $CURRENT_WORKING_DIRECTORY = getcwd();
+
+        chdir($this->rootPath);
+
+        $MAX_EXECUTION_TIME = 1800; // "30 Mins" for slow internet connections.
+
+        set_time_limit($MAX_EXECUTION_TIME);
+
+        shell_exec("php " . $this->binPath . "/composer install");
+
+        chdir($CURRENT_WORKING_DIRECTORY);
+    }
+
+    /**
+     * Determines whether the project has packages to be installed.
+     *
+     * @return bool
+     */
+    public function packagesExists(): bool
+    {
+        return file_exists($this->rootPath . "/composer.json") ? true : false;
+    }
+
+    /**
+     * Determines whether the packages are already installed in the
+     * project.
+     *
+     * @return bool
+     */
+    public function packagesInstalled(): bool
+    {
+        return file_exists($this->rootPath . "/vendor/") ? true : false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Composer
+The instance of Project-Level Composer.
+
+## Credits
+
+- [Abhishek Prakash](https://github.com/abhishek6262)
+
+## Contributing
+Please feel free to fork this package and contribute by submitting a pull request to enhance the functionalities. I will appreciate that a lot. Also please add your name to the credits.
+
+Kindly [follow me on twitter](https://twitter.com/_the_shade)!
+
+## License
+
+The GNU General Public License v3.0. Please see [License File](LICENSE) for more information.
